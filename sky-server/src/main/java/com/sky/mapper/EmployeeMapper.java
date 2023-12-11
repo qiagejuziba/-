36405bb05368f41693d9f539ca714f20b6533341
zhairuihao <hello@zhairuihao.cn>
2023-12-11 22:13:56 +0800

    员工分页查询

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import io.swagger.annotations.ApiOperation;
 import org.apache.ibatis.annotations.Insert;
@@ -23,4 +25,11 @@ public interface EmployeeMapper {
      */
     @Insert("insert into employee (name,username,password,phone,sex,id_number,status,create_time,update_time,create_user,update_user) values (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser}) ")
     void insert(Employee employee);
+
+    /**
+     * 分页查询方法
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
