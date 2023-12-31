673a6c194a16b9ff4619a2484167b9353be39c74
zhairuihao <hello@zhairuihao.cn>
2023-12-31 20:51:42 +0800

    微信支付（跳过支付）功能业务代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -2,6 +2,11 @@ package com.sky.mapper;
 
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -11,4 +16,36 @@ public interface OrderMapper {
      * @param orders
      */
     void insert(Orders orders);
+
+
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+    /**
+     * 用于替换微信支付更新数据库状态的问题
+     * @param orderStatus
+     * @param orderPaidStatus
+     * @param checkoutTime
+     * @param orderNumber
+     */
+    @Update("update orders set status = #{orderStatus},pay_status = #{orderPaidStatus} ,checkout_time = #{checkoutTime} where number = #{orderNumber}")
+    void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime checkoutTime, String orderNumber);
+
+    /**
+     * 根据订单号查询订单信息
+     * @param orders
+     * @return
+     */
+    @Select("select * from orders where number = #{number}")
+    List<Orders> list(Orders orders);
 }
--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,25 +1,25 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sky.constant.MessageConstant;
 import com.sky.context.BaseContext;
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.dto.OrdersSubmitDTO;
-import com.sky.entity.AddressBook;
-import com.sky.entity.OrderDetail;
-import com.sky.entity.Orders;
-import com.sky.entity.ShoppingCart;
+import com.sky.entity.*;
 import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.OrderBusinessException;
 import com.sky.exception.ShoppingCartBusinessException;
-import com.sky.mapper.AddressBookMapper;
-import com.sky.mapper.OrderDetailMapper;
-import com.sky.mapper.OrderMapper;
-import com.sky.mapper.ShoppingCartMapper;
+import com.sky.mapper.*;
 import com.sky.service.OrderService;
+import com.sky.utils.WeChatPayUtil;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +35,12 @@ public class OrderServiceImpl implements OrderService {
     private AddressBookMapper addressBookMapper;
     @Autowired
     private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private WeChatPayUtil weChatPayUtil;
+    @Autowired
+    private UserMapper userMapper;
+    //设置全局变量，跳过微信支付的订单号
+    //public String orderNumber;
 
     /**
      * 用户下订单
@@ -67,11 +73,13 @@ public class OrderServiceImpl implements OrderService {
         orders.setOrderTime(LocalDateTime.now());
         orders.setPayStatus(Orders.UN_PAID);
         orders.setStatus(Orders.PENDING_PAYMENT);
-        orders.setNumber(String.valueOf(System.currentTimeMillis()));
+        orders.setNumber(String.valueOf(System.currentTimeMillis()));//设置订单号
         orders.setPhone(addressBook.getPhone());
         orders.setConsignee(addressBook.getConsignee());
         orders.setUserId(userId);
         orderMapper.insert(orders);
+        //获取订单号
+        //orderNumber = orders.getNumber();
         //3.向订单明细表插入n条数据
         //放入集合批量插入
         List<OrderDetail> orderDetailList = new ArrayList<>();
@@ -93,4 +101,89 @@ public class OrderServiceImpl implements OrderService {
                 .build();
         return orderSubmitVO;
     }
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    public void payment(OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+   /*     // 当前登录用户id
+        Long userId = BaseContext.getCurrentId();
+        User user = userMapper.getById(userId);
+
+       *//* //调用微信支付接口，生成预支付交易单
+        JSONObject jsonObject = weChatPayUtil.pay(
+                ordersPaymentDTO.getOrderNumber(), //商户订单号
+                new BigDecimal(0.01), //支付金额，单位 元
+                "苍穹外卖订单", //商品描述
+                user.getOpenid() //微信用户的openid
+        );
+
+        if (jsonObject.getString("code") != null && jsonObject.getString("code").equals("ORDERPAID")) {
+            throw new OrderBusinessException("该订单已支付");
+        }*//*
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", "ORDERPAID");
+        OrderPaymentVO vo = jsonObject.toJavaObject(OrderPaymentVO.class);
+        vo.setPackageStr(jsonObject.getString("package"));
+
+        //为替代微信支付成功后的数据库订单状态更新，多定义一个方法进行修改
+        Integer OrderPaidStatus = Orders.PAID; //支付状态，已支付
+        Integer OrderStatus = Orders.TO_BE_CONFIRMED;  //订单状态，待接单
+        //发现没有将支付时间 check_out属性赋值，所以在这里更新
+        LocalDateTime checkOutTime = LocalDateTime.now();
+        //更新订单状态
+        orderMapper.updateStatus(OrderStatus, OrderPaidStatus, checkoutTime, orderNumber);*/
+
+        //查询订单状态，若已支付则抛出异常 改造代码
+        Orders order = new Orders();
+        order.setNumber(ordersPaymentDTO.getOrderNumber());
+        List<Orders> orderList = orderMapper.list(order);
+        if (orderList != null && orderList.size() == 1) {
+            order = orderList.get(0);
+            if (order.getPayStatus() == Orders.PAID) {
+                throw new OrderBusinessException(MessageConstant.ORDER_ALREADY_PAID);
+            }
+        } else
+            throw new OrderBusinessException(MessageConstant.ORDER_NOT_FOUND);
+    }
+
+    /**
+     * 支付成功，修改订单状态
+     *
+     * @param orderNumber
+     */
+    public void paySuccess(String orderNumber) {
+
+/*        // 根据订单号查询订单
+        Orders ordersDB = orderMapper.getByNumber(outTradeNo);
+
+        // 根据订单id更新订单的状态、支付方式、支付状态、结账时间
+        Orders orders = Orders.builder()
+                .id(ordersDB.getId())
+                .status(Orders.TO_BE_CONFIRMED)
+                .payStatus(Orders.PAID)
+                .checkoutTime(LocalDateTime.now())
+                .build();
+
+        orderMapper.update(orders);*/
+
+        //改造代码
+        Orders order = Orders.builder()
+                .number(orderNumber)
+                .build();
+        //根据订单id更新订单的状态、支付方式、支付状态、结账时间
+        List<Orders> ordersList = orderMapper.list(order);
+        if (ordersList != null && ordersList.size() == 1) {
+            order = ordersList.get(0);
+            order.setCheckoutTime(LocalDateTime.now());
+            order.setPayStatus(Orders.PAID);
+            order.setStatus(Orders.TO_BE_CONFIRMED);
+            orderMapper.update(order); //修改支付状态、时间、订单状态
+        }
+
+    }
+
 }
